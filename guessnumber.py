7b7e993c45e5d1c75e07573ab4456371e2023960
Yizhen Zhu <jamilazhu@gmail.com>
2019-12-20 17:20:18 +1100

    add commit

--- guessnumber.py
+++ guessnumber.py
@@ -1,15 +1,19 @@
 import random
 
 r = random.randint(1,100)
+count = 0
 while True:
+    count += 1 # count = count + 1 
     num = input('Please enter number: ')
     num = int(num)
     if num == r:
         print('You are correct!!')
+        print('This is your ', count , ' guess')
         break
     elif num > r:
         print('Bigger than the answer!')
     elif num < r:
         print('Smaller than the answer!')
-
+    print('This is your ', count , ' guess')
+    
         
